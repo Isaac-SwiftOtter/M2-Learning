e919104dea727a3f500569f16a07dd9f194ed093
Isaac-SwiftOtter <isaac@swiftotter.com>
2019-02-20 17:25:31 -0500

    Fixing errors in UI grid display, completing attribute action column

---
+++ app/code/Learning/OrderAttributes/Ui/Component/Grid/Column/PostActions.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @by SwiftOtter, Inc., 2/20/19
+ * @website https://swiftotter.com
+ **/
+
+namespace Learning\OrderAttributes\Ui\Component\Grid\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class PostActions extends Column
+{
+    const ATTRIBUTE_EDIT_URL = 'oapage/orderattributes/edit';
+    const ATTRIBUTE_DELETE_URL = 'oapage/orderattributes/delete';
+
+    private $urlBuilder;
+
+    public function __construct(ContextInterface $context, UiComponentFactory $uiComponentFactory, UrlInterface $urlBuilder, array $components = [], array $data = [])
+    {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                $label = $item['attribute_label'];
+
+                $item[$label]['edit'] = [
+                    'href' => $this->urlBuilder->getUrl(self::ATTRIBUTE_EDIT_URL, ['id' => $item['attribute_id']]),
+                    'label' => __('Edit')
+                ];
+
+                $item[$label]['delete'] = [
+                    'href' => $this->urlBuilder->getUrl(self::ATTRIBUTE_DELETE_URL, ['id' => $item['attribute_id']]),
+                    'label' => __('Delete'),
+                    'confirm' => [
+                        'title' => sprintf(__('Delete %s Attribute'), $label),
+                        'message' => sprintf(__('Are you sure you want to delete this attribute: %s'), $label)
+                    ]
+                ];
+            }
+        }
+        return $dataSource;
+    }
+}
